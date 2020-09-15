833358085de679ed22a0f24a7a00df36e454d0d3
Evgeniy  Budaev <ebudaev@iflex.ru>
2020-09-15 18:49:36 +0300

    Решил все задачи 3-module. Тесты успешно все пройдены

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -4,4 +4,20 @@
  */
 function camelize(str) {
   // ваш код...
+  let newArr = []
+  let arr = str.split('-')
+  newArr.push(arr[0])
+  for (let i = 1; i < arr.length; i++) {
+    let caps = capitalize(arr[i])
+    newArr.push(caps)
+  }
+  let result = newArr.join('')
+  return result
+}
+
+function capitalize(string) {
+  if (typeof string !== 'string') {
+    return ''
+  }
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
