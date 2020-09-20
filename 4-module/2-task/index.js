1c6289c1452cab4ee9c9e997752c70bc312b6b07
Evgeniy  Budaev <ebudaev@iflex.ru>
2020-09-20 11:55:53 +0300

    Решил все задачи 4-module. Тесты все пройдены.

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,8 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  for (let tr of table.rows) {
+    let index = tr.rowIndex;
+    tr.cells[index].style.backgroundColor = "red";
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,38 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let tr of table.rows) {
+    let index = tr.rowIndex
+    const tdArray = tr.cells
+
+    for (let td of tdArray) {
+      const tdDataAvailableBoolean = td.dataset.available
+
+      // Проверка на true/false по атрибуту available
+      if (tdDataAvailableBoolean === 'true') {
+        table.rows[index].classList.add('available')
+      } else if (tdDataAvailableBoolean === 'false') {
+        table.rows[index].classList.add('unavailable')
+      }
+
+      // Проверка на male/female в зависимости от значения колонки Gender
+      if (tdArray[2].innerHTML === 'm') {
+        table.rows[index].classList.add('male')
+      } else if (tdArray[2].innerHTML === 'f') {
+        table.rows[index].classList.add('female')
+      }
+
+      // Проверка на возраст в зависимости от значения колонки Age
+      if (tdArray[1].innerHTML < 18) {
+        table.rows[index].style.textDecoration = 'line-through'
+      }
+    }
+
+    // Присваивание атрибута hidden, если атрибута data-available нет вообще
+    if (!tr.classList.contains("available") || !tr.classList.contains("available")) {
+      tr.setAttribute('hidden', '')
+      table.rows[0].removeAttribute('hidden')
+    }
+
+  }
 }
